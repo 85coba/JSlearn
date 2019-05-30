fe404ea5aa272ae805c992e696de2002b3cf2b3a
85coba <85coba@gmail.com>
2019-05-31 01:39:49 +0300

    Task 7 part 4

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+let btnStartCount = document.getElementById('start'),
+divResultValue = document.querySelectorAll('.result-table [class *= "-value"]'),
+expensesItem = document.getElementsByClassName("expenses-item");
+btnGet1 = document.getElementsByTagName('button')[0],
+btnGet2 = document.getElementsByTagName('button')[1],
+btnCount = document.getElementsByTagName('button')[2],
+inputOptExpenses = document.querySelectorAll('.optionalexpenses-item');
+inputIncome = document.getElementById('income'),
+checkSavings = document.getElementById('savings'),
+inputSavingsSum = document.getElementById('sum'),
+inputSavingsPercent = document.getElementById('percent'),
+inputDateValue = document.querySelectorAll('.time-data [class *= "-value"]')
+
+
+console.log (inputDateValue);
